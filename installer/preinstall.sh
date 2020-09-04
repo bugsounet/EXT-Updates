da46b2dbd21270998c90fc04b4967c9639a46e0a
bugsounet <bugsounet@bugsnana.fr>
2020-09-04 23:31:18 +0200

    test setup

---
+++ installer/preinstall.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# +-----------------+
+# | npm postinstall |
+# | @bugsounet      |
+# +-----------------+
+
+# get the installer directory
+Installer_get_current_dir () {
+  SOURCE="${BASH_SOURCE[0]}"
+  while [ -h "$SOURCE" ]; do
+    DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+    SOURCE="$(readlink "$SOURCE")"
+    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+  done
+  echo "$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+}
+
+Installer_dir="$(Installer_get_current_dir)"
+
+# move to installler directory
+cd "$Installer_dir"
+
+source utils.sh
+
+# module name
+Installer_module="UpdateNotification"
+
+# Let's start !
+Installer_info "Welcome to $Installer_module"
+
+echo
+
+# Check not run as root
+if [ "$EUID" -eq 0 ]; then
+  Installer_error "npm install must not be used as root"
+  exit 1
+fi
+
+# switch branch
+Installer_info "Installing Beta2 Sources..."
+git checkout -f beta2 2>/dev/null || Installer_error "Installing Error !"
+git pull 2>/dev/null
+
+echo
+Installer_info "Installing all library..."
+echo
