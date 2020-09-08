058a2adf6e9f53118a1db6826e3e642228512d49
Cédric <bugsounet26@gmail.com>
2020-09-08 03:35:56 +0200

    Update preinstall.sh

--- installer/preinstall.sh
+++ installer/preinstall.sh
@@ -37,7 +37,7 @@ if [ "$EUID" -eq 0 ]; then
 fi
 
 # switch branch
-Installer_info "Installing Beta3 Sources..."
+Installer_info "Installing Sources..."
 git checkout -f prod 2>/dev/null || Installer_error "Installing Error !"
 git pull 2>/dev/null
 
